acc261c8e10cfc99d2ea230370f341e1e9c19100
Poobathi <boopathy.s@gmail.com>
2020-05-09 14:27:07 +0530

    v0.16

--- myapp.js
+++ myapp.js
@@ -8,8 +8,9 @@ const mongoose = require("mongoose");
 
 mongoose
   .connect(
-    "mongodb+srv://" + process.env.MONGODB_DBUSER + ":" + process.env.MONGODB_DBPWD + "@" + process.env.MONGODB_CLUSTER + "/" + process.env.MONGODB_DBNAME + "?retryWrites=true&w=majority",
-    {useNewUrlParser: true, useUnifiedTopology: true} //added as per deprication warnings
+    //"mongodb+srv://" + process.env.MONGODB_DBUSER + ":" + process.env.MONGODB_DBPWD + "@" + process.env.MONGODB_CLUSTER + "/" + process.env.MONGODB_DBNAME + "?retryWrites=true&w=majority",
+    //{useNewUrlParser: true, useUnifiedTopology: true} //added as per deprication warnings
+    "mongodb+srv://dbuser:dbpwd09@boomongocluster-rcqr2.azure.mongodb.net/node-angular?retryWrites=true&w=majority",
   )
     .catch(err => {
         handleError(err)
