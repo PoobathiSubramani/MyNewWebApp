47926c900a5266f05785435d44ce3c0d9f851461
Poobathi <boopathy.s@gmail.com>
2020-05-09 11:27:53 +0530

    v0.14

--- myapp.js
+++ myapp.js
@@ -5,19 +5,13 @@ const dbConnectionStatus = "not connected to DB";
 
 
 const mongoose = require("mongoose");
+
 mongoose
   .connect(
     "mongodb+srv://" + process.env.MONGODB_DBUSER + ":" + process.env.MONGODB_DBPWD + "@" + process.env.MONGODB_CLUSTER + "/" + process.env.MONGODB_DBNAME + "?retryWrites=true&w=majority",
     {useNewUrlParser: true, useUnifiedTopology: true} //added as per deprication warnings
   )
-  .then(() => {
-    console.log("Connected to database!");
-    dbConnectionStatus = "DB connected"
-  })
-  .catch(() => {
-    console.log("DB Connection failed!");
-  })
-
+    .catch(err => handleError(err));
 
 myapp.get("", (req, res) => {
     res.send({Message:"Server initialized and " + dbConnectionStatus});
