3d813c5dd81f44e51b170371f7940eaba4ae2922
Poobathi <boopathy.s@gmail.com>
2020-05-09 11:02:58 +0530

    v0.10

--- myapp.js
+++ myapp.js
@@ -4,7 +4,7 @@ const myapp = express();
 
 
 myapp.get("", (req, res) => {
-    res.send({Message:"Hai...how are you?" + MONGODB_CLUSTER});
+    res.send({Message:"Hai...how are you?" + process.env.MONGODB_CLUSTER});
 })
 
 module.exports=myapp;
