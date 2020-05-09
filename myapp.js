3726ae1df41ae7b07292f42af3556a97e3ab5ba3
Poobathi <boopathy.s@gmail.com>
2020-05-09 11:10:50 +0530

    v0.11
    adding db conn

--- myapp.js
+++ myapp.js
@@ -2,6 +2,19 @@ const express = require("express");
 const myapp = express();
 
 
+const mongoose = require("mongoose");
+mongoose
+  .connect(
+    "mongodb+srv://" + process.env.MONGODB_DBUSER + ":" + process.env.MONGODB_DBPWD + "@" + process.env.MONGODB_CLUSTER + "/" + process.env.MONGODB_DBNAME + "?retryWrites=true&w=majority",
+    {useNewUrlParser: true, useUnifiedTopology: true} //added as per deprication warnings
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("DB Connection failed!");
+  })
+
 
 myapp.get("", (req, res) => {
     res.send({Message:"Hai...how are you?" + process.env.MONGODB_CLUSTER});
