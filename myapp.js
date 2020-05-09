adf1f620f0bb635f5777daf0c8ca81691fe92465
Poobathi <boopathy.s@gmail.com>
2020-05-09 10:58:07 +0530

    v0.08
    testing env variables

--- myapp.js
+++ myapp.js
@@ -2,8 +2,9 @@ const express = require("express");
 const myapp = express();
 
 
+
 myapp.get("", (req, res) => {
-    res.send({Message:"Hai...how are you?"});
+    res.send({Message:"Hai...how are you?" + process.env.TEST});
 })
 
 module.exports=myapp;
