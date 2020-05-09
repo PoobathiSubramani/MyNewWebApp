613d292af1f464bb7d033cc72b2058459498cf20
Poobathi <boopathy.s@gmail.com>
2020-05-09 11:33:01 +0530

    v0.15

--- myapp.js
+++ myapp.js
@@ -11,7 +11,10 @@ mongoose
     "mongodb+srv://" + process.env.MONGODB_DBUSER + ":" + process.env.MONGODB_DBPWD + "@" + process.env.MONGODB_CLUSTER + "/" + process.env.MONGODB_DBNAME + "?retryWrites=true&w=majority",
     {useNewUrlParser: true, useUnifiedTopology: true} //added as per deprication warnings
   )
-    .catch(err => handleError(err));
+    .catch(err => {
+        handleError(err)
+        console.log(err);
+    });
 
 myapp.get("", (req, res) => {
     res.send({Message:"Server initialized and " + dbConnectionStatus});
